af0d0dc4a7c613af340da6625c6f7b649aebfed0
Unknown <fatihb@live.be>
2019-03-24 16:18:37 +0100

    Initialized Hidor class in Helpers folder

---
+++ WebEncryptor/Helpers/Hidor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebEncryptor.Helpers
+{
+    public class Hidor
+    {
+        //Steganography class
+    }
+}
