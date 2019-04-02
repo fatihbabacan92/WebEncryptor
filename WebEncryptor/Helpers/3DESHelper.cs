bb4a548bcb21bf08e239c04aac5a65d9d085f532
KevinDominguez <dominguez-kevin-1995@hotmail.com>
2019-04-02 12:20:49 +0200

    added hybrid crypto helper
    added hybrid crypto helper

--- WebEncryptor/Helpers/3DESHelper.cs
+++ WebEncryptor/Helpers/3DESHelper.cs
@@ -56,5 +56,31 @@ namespace WebEncryptor.Helpers
             }
         }
 
+        public static byte[] Decrypt(byte[] toDecryptArray, byte[] keyArray)
+        {
+            TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider();
+            //set the secret key for the tripleDES algorithm
+            tdes.Key = keyArray;
+            //mode of operation. there are other 4 modes. 
+            //We choose ECB(Electronic code Book)
+
+            tdes.Mode = CipherMode.ECB;
+            //padding mode(if any extra byte added)
+            tdes.Padding = PaddingMode.PKCS7;
+
+            ICryptoTransform cTransform = tdes.CreateDecryptor();
+            byte[] resultArray = cTransform.TransformFinalBlock(
+                toDecryptArray, 0, toDecryptArray.Length);
+            //Release resources held by TripleDes Encryptor                
+            tdes.Clear();
+            //return the Clear decrypted TEXT
+            return resultArray;
+        }
+
+        public byte[] GetKey()
+        {
+            return tdes.Key;
+        }
+
     }
 }
---
+++ WebEncryptor/Helpers/HybridCryptoHelper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebEncryptor.Helpers
+{
+    public class HybridCryptoHelper
+    {
+    }
+}
