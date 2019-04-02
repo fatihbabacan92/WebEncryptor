82759a51c6827a85fa406ccd8cd9db7c576dbb8c
Unknown <fatihb@live.be>
2019-04-02 12:10:47 +0200

    Added encrypt and decrypt methods to Hidor class

--- WebEncryptor/Controllers/HidorController.cs
+++ WebEncryptor/Controllers/HidorController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -11,6 +12,17 @@ namespace WebEncryptor.Controllers
         public IActionResult Hidor()
         {
             return View();
+        }
+
+        public Bitmap HideText(Bitmap btm, String text)
+        {
+            Bitmap hidden = WebEncryptor.Helpers.Hidor.embedText(text, btm);
+            return hidden;
+        }
+
+        public String ExtractText(Bitmap btm)
+        {
+            return WebEncryptor.Helpers.Hidor.extractText(btm);
         }
     }
 }
