904912213d7ad21952cc4ddd1c31b9a5b3d12417
KevinDominguez <dominguez-kevin-1995@hotmail.com>
2019-04-02 12:15:38 +0200

    Update 3DESHelper.cs
    added encrypt and decrypt

--- WebEncryptor/Helpers/3DESHelper.cs
+++ WebEncryptor/Helpers/3DESHelper.cs
@@ -1,11 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
 
 namespace WebEncryptor.Helpers
 {
     public class _3DesHelper
     {
+        public TripleDESCryptoServiceProvider tdes { get; set; }
+
+        public _3DesHelper()
+        {
+            tdes = new TripleDESCryptoServiceProvider();
+        }
+
+        public static Dictionary<string, byte[]> Encrypt(byte[] toEncryptArray)
+        {
+            Dictionary<string, byte[]> output = new Dictionary<string, byte[]>();
+            byte[] keyArray;
+
+            // Generate key (never weak key)
+            TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider();
+            tdes.GenerateKey();
+
+            keyArray = tdes.Key;
+
+            //mode of operation. there are other 4 modes.
+            //We choose ECB(Electronic code Book)
+            tdes.Mode = CipherMode.ECB;
+            //padding mode(if any extra byte added)
+
+            tdes.Padding = PaddingMode.PKCS7;
+
+            ICryptoTransform cTransform = tdes.CreateEncryptor();
+            //transform the specified region of bytes array to resultArray
+            try
+            {
+                byte[] resultArray =
+                    cTransform.TransformFinalBlock(toEncryptArray, 0,
+                        toEncryptArray.Length);
+                //Release resources held by TripleDes Encryptor
+                tdes.Clear();
+
+                //Add key and encrypted data to output Dictionary
+                output.Add("text", resultArray);
+                output.Add("key", keyArray);
+
+                //Return the encrypted data into unreadable string format
+                return output;
+            }
+            catch (NullReferenceException ex)
+            {
+                throw new NullReferenceException(ex.Message);
+            }
+        }
+
     }
 }
