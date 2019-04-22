b7c89c82727714cfff6d96454998c1422585b955
Fatih <fatihb@live.be>
2019-04-22 17:58:54 +0200

    Created upload function in HidorController
    Bugs need to be solved.

--- WebEncryptor/Controllers/HidorController.cs
+++ WebEncryptor/Controllers/HidorController.cs
@@ -1,26 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebEncryptor.Models;
 
 namespace WebEncryptor.Controllers
 {
     public class HidorController : Controller
     {
+
+        Bitmap btm;
+
         public IActionResult Hidor()
         {
             return View();
         }
 
-        public Bitmap HideText(Bitmap btm, String text)
+        public async void Upload(ImageFile image)
+        {
+            if (ModelState.IsValid)
+            {
+                if (image != null && image.Length > 0)
+                {
+                    var fileName = Path.GetFileName(image.FileName);
+                    var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\uploads", fileName);
+                    using (var fileSteam = new FileStream(filePath, FileMode.Create))
+                    {
+                        await image.CopyToAsync(fileSteam);
+                    }
+                    btm = new Bitmap(filePath);
+                }
+            }
+        }
+
+        public Bitmap HideText(String text)
         {
             Bitmap hidden = WebEncryptor.Helpers.Hidor.embedText(text, btm);
             return hidden;
         }
 
-        public String ExtractText(Bitmap btm)
+        public String ExtractText()
         {
             return WebEncryptor.Helpers.Hidor.extractText(btm);
         }
